f432d32cb24da64ff03bf60c9bba0188acab9d8f
David Baron <davidbaron@mycodingsoft.com>
2020-02-27 18:25:02 +0100

    Init project

--- src/AbstractManager.php
+++ src/AbstractManager.php
@@ -1,6 +1,14 @@
 <?php 
 namespace Lite\Core;
+
+use Lite\Core\Factory\PdoFactory;
+
 class AbstractManager
 {
-    
+
+    public function db()
+    {
+        $connection = new PdoFactory("mysql","espace_client","localhost:3308", "root", "");
+        return $connection->pdo();
+    }
 }
--- src/Factory/PdoFactory.php
+++ src/Factory/PdoFactory.php
@@ -2,5 +2,17 @@
 namespace Lite\Core\Factory;
 class PdoFactory
 {
-    
+    protected $pdo;
+
+    public function __construct(string $dbms, string $dbname, string $host, string $user, string $pass)
+    {
+        $this->pdo = new \PDO("$dbms:dbname=$dbname;host=$host", $user, $pass);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+    }
+
+    public function pdo()
+    {
+        return $this->pdo;
+    }
 }
