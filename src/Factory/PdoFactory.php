532b4225280e98997110662ba70c4753ecd7f149
David Baron <davidbaron@mycodingsoft.com>
2020-02-27 19:59:47 +0100

    Init project

--- src/Factory/PdoFactory.php
+++ src/Factory/PdoFactory.php
@@ -8,7 +8,7 @@ class PdoFactory
     {
         $this->pdo = new \PDO("$dbms:dbname=$dbname;host=$host", $user, $pass);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+        //$this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
     }
 
     public function pdo()
